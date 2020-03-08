99d762bab1c34af60e0b1d6e2d81f36cb3d654ff
HikaruEgashira <shighten@gmail.com>
2020-03-08 17:25:06 +0900

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# optool
+
+A command-line tool for calc
+
+```
+optool 0.1.0
+A command-line tool for string substitutions
+
+USAGE:
+    optool <op> <a> <b>
+
+OPTIONS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+ARGS:
+    <op>    operator
+    <a>     number
+    <b>     number
+```
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use clap::{crate_description, crate_name, crate_version, App, AppSettings, Arg};
+
+#[derive(Debug, Clone)]
+struct Application<'a> {
+    a: &'a str,
+    b: &'a str,
+    op: &'a str,
+}
+
+impl<'a> Application<'a> {
+    fn run(&self) {
+        let a: i32 = self.a.parse().unwrap();
+        let b: i32 = self.b.parse().unwrap();
+
+        match self.op {
+            "Add" => println!("{}", a + b),
+            "Sub" => println!("{}", a - b),
+            "Mul" => println!("{}", a * b),
+            "Div" => println!("{}", a / b),
+            _ => println!("Error op can available Add Sub Mul Div"),
+        }
+    }
+}
+
+fn main() {
+    let app = App::new(crate_name!())
+        .version(crate_version!())
+        .about(crate_description!())
+        .global_setting(AppSettings::ColoredHelp)
+        .global_setting(AppSettings::UnifiedHelpMessage)
+        .arg(Arg::with_name("op").required(true).help("operator"))
+        .arg(Arg::with_name("a").required(true).help("number"))
+        .arg(Arg::with_name("b").required(true).help("number"));
+
+    let matches = app.get_matches();
+
+    let app = Application {
+        op: matches.value_of("op").expect("required argument"),
+        a: matches.value_of("a").expect("required argument"),
+        b: matches.value_of("b").expect("required argument"),
+    };
+    app.run();
+}
